136b252e94b6c5f6e1e51400bceb6ab6f4b4a0a4
KarenVelasquezCAMPUS <karenvelasquez1009@gmail.com>
2023-10-04 07:46:24 -0500

    feat: :sparkles: sobrecarga de la creacion de los modelos
    se refleja a la hora de generar las tablas, todo el proceso se hizo a su mayoria con refactor

--- Infrastructure/Data/ApiVeterinariaContext.cs
+++ Infrastructure/Data/ApiVeterinariaContext.cs
@@ -1,9 +1,16 @@
+using System.Reflection;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Data;
 public class ApiVeterinariaContext : DbContext
 {
     public ApiVeterinariaContext(DbContextOptions options) : base(options)
+    { 
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
     }
 }
