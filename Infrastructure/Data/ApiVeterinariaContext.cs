653fd2bd6e15402319a434d4b32a8166f45ea1ef
KarenVelasquezCAMPUS <karenvelasquez1009@gmail.com>
2023-10-04 07:41:09 -0500

    feat: :sparkles: constructor de la db
    se añadio le constructor (refactor) en el dbset

--- Infrastructure/Data/ApiVeterinariaContext.cs
+++ Infrastructure/Data/ApiVeterinariaContext.cs
@@ -1,10 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Data;
 public class ApiVeterinariaContext : DbContext
 {
+    public ApiVeterinariaContext(DbContextOptions options) : base(options)
+    {
+    }
 }
